64197d7077a167b2cd0e17ffee5455117d04a85a
adam <jendrzejczak.adam@gmail.com>
2025-07-07 11:50:44 +0200

    first commit

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-header',
+    standalone: true, // co to? standalone vs modular.
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.css']    
+})
+export class HeaderComponent {}
